a07e6c232240b36738538bceb82dd584efa9ea82
Echo <819905193@qq.com>
2025-03-03 23:50:58 +0800

    Bug Fixed

--- backend/src/main/java/com/edu/service/UserService.java
+++ backend/src/main/java/com/edu/service/UserService.java
@@ -3,6 +3,6 @@ package com.edu.service;
 import com.edu.entity.account.User;
 
 public interface UserService {
-    public User findUserById(String userId);
-    public boolean validateUser(String userId, String password);
+    User findUserById(String userId);
+    boolean validateUser(String userId, String password);
 }
