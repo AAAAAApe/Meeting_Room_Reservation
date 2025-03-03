4d142fc38940e461ab4692097c3cfcc22ebc21ec
Echo <819905193@qq.com>
2025-03-03 23:50:58 +0800

    Bug Fixed

--- backend/src/main/java/com/edu/mapper/UserMapper.java
+++ backend/src/main/java/com/edu/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.edu.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.edu.entity.account.User;
-import org.apache.ibatis.annotations.Select;
 
 public interface UserMapper extends BaseMapper<User> {
 
