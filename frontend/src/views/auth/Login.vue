164d7747ff3f7c73275fed57f5e83496027ce1fc
Echo <819905193@qq.com>
2025-03-07 17:37:40 +0800

    feat(auth): add login functionality with axios post request

--- frontend/src/views/auth/Login.vue
+++ frontend/src/views/auth/Login.vue
@@ -11,7 +11,7 @@
                 <el-input type="password" placeholder="请输入密码" v-model="loginForm.password" />
               </el-form-item>
 
-              <el-button type="primary" @click="">登陆</el-button>
+              <el-button type="primary" @click="handleLogin">登陆</el-button>
             </el-form>
 
         </div>
@@ -20,11 +20,23 @@
 
 <script setup lang="ts">
     import { ref } from 'vue'
+    import axios from 'axios'
     
     const loginForm = ref({
       username: '',
       password: ''
     })
+
+    const handleLogin = async () => {
+      try {
+        const response = await axios.post('/login', {
+          userId: loginForm.value.username,
+          password: loginForm.value.password
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    }
 </script>
 
 <style>
