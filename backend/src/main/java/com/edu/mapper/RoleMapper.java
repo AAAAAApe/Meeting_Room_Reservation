42f729cf37d733633258415bba99fdcc3eb71262
Echo <819905193@qq.com>
2025-03-12 22:48:51 +0800

    feat(mapper): 新增 RoleMapper 接口，继承自 BaseMapper<Role>，用于角色数据的持久化操作

---
+++ backend/src/main/java/com/edu/mapper/RoleMapper.java
@@ -0,0 +1,9 @@
+package com.edu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.entity.account.Role;
+import com.edu.entity.account.User;
+import org.apache.ibatis.annotations.Param;
+
+public interface RoleMapper extends BaseMapper<Role> {
+}
