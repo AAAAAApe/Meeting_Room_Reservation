05773063bc3a1a2565cae917dc0945adb1b628d1
Echo <819905193@qq.com>
2025-03-12 22:50:30 +0800

    feat(service): 更新 SequenceService 接口，新增生成用户ID的方法及其详细注释

--- backend/src/main/java/com/edu/service/SequenceService.java
+++ backend/src/main/java/com/edu/service/SequenceService.java
@@ -3,6 +3,20 @@ package com.edu.service;
 import com.edu.entity.sequence.Sequence;
 import com.github.jeffreyning.mybatisplus.service.IMppService;
 
+/**
+ * 序列号服务接口
+ * 用于生成用户ID的序列号管理服务，继承自IMppService以支持多主键操作
+ */
 public interface SequenceService extends IMppService<Sequence> {
+    /**
+     * 生成用户ID
+     * 根据年份和角色名生成唯一的用户ID，格式为：年份+角色ID+4位序号
+     * 例如：2023010001 表示2023年第一个教师账号
+     *
+     * @param year 年份
+     * @param role 角色名称
+     * @return 生成的用户ID字符串
+     * @throws com.edu.exception.RoleNotFoundException 当指定的角色名不存在时抛出此异常
+     */
     String generateUserId(int year, String role);
 }
