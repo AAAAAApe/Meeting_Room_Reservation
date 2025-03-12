6ca1e2f94d1ea386b94a16860697df57857ef3ce
Echo <819905193@qq.com>
2025-03-12 22:49:13 +0800

    feat(mapper): 新增 TeacherViewMapper 接口，继承自 BaseMapper<TeacherView>，用于教师视图数据的持久化操作

---
+++ backend/src/main/java/com/edu/mapper/TeacherViewMapper.java
@@ -0,0 +1,6 @@
+package com.edu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.entity.view.TeacherView;
+
+public interface TeacherViewMapper extends BaseMapper<TeacherView> {} 
