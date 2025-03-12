5d0b254578d0e3d5808f2d8edff406d0fedaee04
Echo <819905193@qq.com>
2025-03-12 22:18:25 +0800

    fix(entity): 修改 Sequence 类中的 role 字段为 role_id，并更新用户ID格式，以提高数据一致性

--- backend/src/main/java/com/edu/entity/sequence/Sequence.java
+++ backend/src/main/java/com/edu/entity/sequence/Sequence.java
@@ -8,14 +8,14 @@ import lombok.Data;
 @Data
 @TableName("sequence")
 public class Sequence {
-    // userId format: year+role(to int)+currentValue
+    // userId format: year+role_id(to int)+currentValue
     // example: 2023+1+0001 = 2023010001 means the first teacher in 2023
     @MppMultiId
     @TableField(value = "year")
     private Integer year;
     @MppMultiId
-    @TableField(value = "role")
-    private String role;
+    @TableField(value = "role_id")
+    private Byte roleId;
     @TableField(value = "current_value")
     private Integer currentValue;
 }
