9f4b455d8357774559100de9172e3ba0746a545c
Echo <819905193@qq.com>
2025-03-12 22:49:20 +0800

    feat(mapper): 更新 UserMapper 接口，修改用户信息获取方法，返回 UserView 对象以支持用户视图数据的持久化操作

--- backend/src/main/java/com/edu/mapper/UserMapper.java
+++ backend/src/main/java/com/edu/mapper/UserMapper.java
@@ -1,11 +1,10 @@
 package com.edu.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.edu.dto.UserDTO;
 import com.edu.entity.account.User;
+import com.edu.entity.view.UserView;
 import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper extends BaseMapper<User> {
-    UserDTO getStudentInfo(@Param("userId") String userId);
-    UserDTO getTeacherInfo(@Param("userId") String userId);
+    UserView getUserInfoById(@Param("userId") String userId);
 }
