805c584c48b3b03092d368ebe44e973dffc342e7
Echo <819905193@qq.com>
2025-03-16 00:21:39 +0800

    删除 RoleMapper 接口中的未使用导入，简化代码结构

--- backend/src/main/java/com/edu/mapper/RoleMapper.java
+++ backend/src/main/java/com/edu/mapper/RoleMapper.java
@@ -2,8 +2,6 @@ package com.edu.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.edu.entity.account.Role;
-import com.edu.entity.account.User;
-import org.apache.ibatis.annotations.Param;
 
 public interface RoleMapper extends BaseMapper<Role> {
 }
