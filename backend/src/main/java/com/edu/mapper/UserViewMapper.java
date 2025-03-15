8c3c6efd0824baac9424b1219b09d35d9bab4e70
Echo <819905193@qq.com>
2025-03-16 00:23:16 +0800

    feat(mapper): 新增 UserViewMapper 接口，提供对 UserView 实体的基本 CRUD 操作

---
+++ backend/src/main/java/com/edu/mapper/UserViewMapper.java
@@ -0,0 +1,8 @@
+package com.edu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.entity.view.UserView;
+
+public interface UserViewMapper extends BaseMapper<UserView>{
+
+}
