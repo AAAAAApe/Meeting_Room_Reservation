2aac101529db5cd2446a12c63cf83efc2b016a83
Echo <819905193@qq.com>
2025-03-16 00:21:16 +0800

    删除 UserMapper 接口中的 getUserInfoById 方法，简化用户映射逻辑

--- backend/src/main/java/com/edu/mapper/UserMapper.java
+++ backend/src/main/java/com/edu/mapper/UserMapper.java
@@ -2,9 +2,6 @@ package com.edu.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.edu.entity.account.User;
-import com.edu.entity.view.UserView;
-import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper extends BaseMapper<User> {
-    UserView getUserInfoById(@Param("userId") String userId);
 }
