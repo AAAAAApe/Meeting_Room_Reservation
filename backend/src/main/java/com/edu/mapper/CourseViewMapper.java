7ebf373d4b456df7e6ab7108b716341394474b50
Echo <819905193@qq.com>
2025-03-20 01:26:57 +0800

    feat: 新增CourseViewMapper和CourseSelectViewMapper接口
    添加了两个新的Mapper接口，用于处理CourseView和CourseSelectView的数据库操作

---
+++ backend/src/main/java/com/edu/mapper/CourseViewMapper.java
@@ -0,0 +1,7 @@
+package com.edu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.entity.view.CourseView;
+
+public interface CourseViewMapper extends BaseMapper<CourseView> {
+}
